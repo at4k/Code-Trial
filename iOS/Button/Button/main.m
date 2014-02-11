33b93f3595bad08ce4e597b634f7a9e978f1a308
Atsushi K <at4k@users.noreply.github.com>
2014-02-11 12:22:05 +0900

    Initial Commit

---
+++ iOS/Button/Button/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Button
+//
+//  Created by Atsushi Kajita on 2014/02/11.
+//  Copyright (c) 2014年 at4k. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CodeTrialAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([CodeTrialAppDelegate class]));
+    }
+}
